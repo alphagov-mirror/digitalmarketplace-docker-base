e9ee993d28672426bdac148f733f782f38cf6379
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2019-01-17 14:04:50 +0000

    Add script to make it easy to scan Docker images using Snyk

---
+++ docker-scan.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+colorecho()
+{
+	tput setaf 6
+	echo "$@"
+	tput sgr0
+}
+
+DOCKER_REPO="$1"
+DOCKER_FILE="$2"
+
+# create a clean directory for output files
+TMPDIR="$(mktemp -d /tmp/docker-scan.XXX)"
+trap 'rm -r "${TMPDIR}"' EXIT
+
+cp "${DOCKER_FILE}" "${TMPDIR}"
+DOCKER_FILE="$(basename ${DOCKER_FILE})"
+
+# get SNYK_TOKEN from credentials repo
+eval "$(${DM_CREDENTIALS_REPO}/sops-wrapper -d ${DM_CREDENTIALS_REPO}/jenkins-vars/snyk_credentials.env)"
+if [ -n SNYK_TOKEN ]; then
+	>&2 echo "Error: sops failed getting SNYK_TOKEN"
+	exit 2
+fi
+SNYK_ORG="${SNYK_ORG:-digital-marketplace}"
+
+docker run --rm \
+	-e MONITOR="${MONITOR}" \
+	-e SNYK_TOKEN="${SNYK_TOKEN}" \
+	-v "${TMPDIR}":/project:rw \
+	-v /var/run/docker.sock:/var/run/docker.sock \
+	digitalmarketplace/snyk-cli:docker \
+	test \
+	--json \
+	--org="${SNYK_ORG}" \
+	--docker "${DOCKER_REPO}" \
+	--file="${DOCKER_FILE}" \
+	>/dev/null \
+	2>/dev/null \
+;
+STATUS="$?"
+
+if [ -n "${OUTDIR}" ] && [ -d "${OUTDIR}" ]; then
+	(cd "${TMPDIR}" && cp snyk_report.css snyk_report.html snyk-result.json snyk-error.log "${OUTDIR}")
+fi
+
+if [ $STATUS -eq 1 ]; then
+	COUNT="$(jq '.uniqueCount' ${TMPDIR}/snyk-result.json)"
+	colorecho "Snyk found ${COUNT} vulns in ${DOCKER_REPO}. Uh-oh!"
+elif [ $STATUS -eq 0 ]; then
+	colorecho "Snyk gave ${DOCKER_REPO} a clean bill of health. You're good to go!"
+fi
