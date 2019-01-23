a63ff5cdc0340f15633a8bff9434c0c9622f1705
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2019-01-23 11:33:54 +0000

    Improve ergonomics of docker scan script

--- docker-scan.sh
+++ docker-scan.sh
@@ -7,6 +7,34 @@ colorecho()
 	tput sgr0
 }
 
+usage()
+{
+cat <<EOF
+Usage: docker-scan [-o OUTDIR] DOCKER_REPO DOCKER_FILE
+
+ -o OUTDIR  Copy report files to OUTDIR.
+            This will overwrite existing files.
+EOF
+}
+
+while getopts 'o:' opt; do
+	case $opt in
+		o)
+			OUTDIR="$OPTARG"
+			;;
+		*)
+			usage
+			exit 2
+			;;
+	esac
+done
+
+shift $((OPTIND-1))
+if [ $# -ne 2 ]; then
+	usage
+	exit 2
+fi
+
 DOCKER_REPO="$1"
 DOCKER_FILE="$2"
 
@@ -17,14 +45,17 @@ trap 'rm -rf "${TMPDIR}"' EXIT
 cp "${DOCKER_FILE}" "${TMPDIR}"
 DOCKER_FILE="$(basename ${DOCKER_FILE})"
 
-# get SNYK_TOKEN from credentials repo
-eval "$(${DM_CREDENTIALS_REPO}/sops-wrapper -d ${DM_CREDENTIALS_REPO}/jenkins-vars/snyk_credentials.env)"
 if [ -z "$SNYK_TOKEN" ]; then
-	>&2 echo "Error: sops failed getting SNYK_TOKEN"
-	exit 2
+	# get SNYK_TOKEN from credentials repo
+	eval "$(${DM_CREDENTIALS_REPO}/sops-wrapper -d ${DM_CREDENTIALS_REPO}/jenkins-vars/snyk_credentials.env)"
+	if [ -z "$SNYK_TOKEN" ]; then
+		>&2 echo "Error: sops failed getting SNYK_TOKEN"
+		exit 2
+	fi
 fi
 SNYK_ORG="${SNYK_ORG:-digital-marketplace}"
 
+echo "Scanning ${DOCKER_REPO} with Snyk..."
 docker run --rm \
 	-e MONITOR="${MONITOR}" \
 	-e SNYK_TOKEN="${SNYK_TOKEN}" \
@@ -41,7 +72,8 @@ docker run --rm \
 ;
 STATUS="$?"
 
-if [ -n "${OUTDIR}" ] && [ -d "${OUTDIR}" ]; then
+if [ -n "${OUTDIR}" ] ; then
+	mkdir -p ${OUTDIR}
 	echo "Copying report files to ${OUTDIR}"
 	cp "${TMPDIR}/snyk_report.css" "${TMPDIR}/snyk_report.html" "${TMPDIR}/snyk-result.json" "${TMPDIR}/snyk-error.log" "${OUTDIR}"
 fi
