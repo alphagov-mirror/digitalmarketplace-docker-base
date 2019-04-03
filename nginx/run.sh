5d0c14b9eff4a6b6bef85ebb2f5644038636753e
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2019-04-03 09:55:33 +0100

    Use sed to find-replace PORT envvar in nginx conf
    Nginx does not allow using environment variables in server blocks[1], so
    instead we use sed to find instances of the template variable {PORT} and
    replace with the value of the envvar.
    
    We do this at runtime rather than at image build time, as the PORT
    envvar is usually set by the Cloud Foundry runtime[2].
    
    [1]: https://stackoverflow.com/a/21886461
    [2]: https://docs.run.pivotal.io/devguide/deploy-apps/environment-variable.html#PORT

--- nginx/run.sh
+++ nginx/run.sh
@@ -3,5 +3,7 @@
 echo "${PROXY_AUTH_CREDENTIALS}" >> /etc/nginx/.htpasswd
 
 sed -i "s/{DM_APP_NAME}/$DM_APP_NAME/g" /etc/nginx/nginx.conf
+[ -f /etc/nginx/sites-enabled/api ] && sed -i "s/{PORT}/$PORT/g" /etc/nginx/sites-enabled/api
+[ -f /etc/nginx/sites-enabled/frontend ] && sed -i "s/{PORT}/$PORT/g" /etc/nginx/sites-enabled/frontend
 
 exec /usr/sbin/nginx
