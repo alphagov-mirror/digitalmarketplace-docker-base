089cce6a353a626c49b68066248a9285bbb3b756
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2019-04-03 10:03:21 +0100

    Tweak Nginx run script to fail early
    If we have not set an environment variable needed by Nginx we want to
    know about it loudly and clearly.

--- nginx/run.sh
+++ nginx/run.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+[ "${PROXY_AUTH_CREDENTIALS}x" = "x" ] && echo 'envvar $PROXY_AUTH_CREDENTIALS not set' && exit 1
+[ "${DM_APP_NAME}x" = "x" ] && echo 'envvar $DM_APP_NAME not set' && exit 1
+[ "${PORT}x" = "x" ] && echo 'envvar $PORT not set' && exit 1
+
 echo "${PROXY_AUTH_CREDENTIALS}" >> /etc/nginx/.htpasswd
 
 sed -i "s/{DM_APP_NAME}/$DM_APP_NAME/g" /etc/nginx/nginx.conf
